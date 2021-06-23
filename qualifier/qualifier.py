f8c29560335e149730388638aa4217928357d877
CiaranSanders <ciaransanders@gmail.com>
2021-06-22 21:52:08 -0700

    headers working right now, needs some testing; also, build_rows is just build_headers, right?

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -12,54 +12,64 @@ def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, center
     :param centered: If the items should be aligned to the center, else they are left aligned.
     :return: A table representing the rows passed in.
     """
-    def find_column_lengths(rows: List[List[Any]]) -> List[int]:
-        """
-        Find the length of the largest element in each column of the parameter rows.
-        :return: A list of the longest lengths for each row (indexes matching).
-        """
-        col_lengths = [0] * len(rows[0])
-        for j in range(len(rows[0])):
-            for i in range(len(rows)):
-                if len(rows[i][j]) > col_lengths[j]:
-                    col_lengths[j] = len(rows[i][j])
-        return col_lengths
 
-    def build_top_line(col_lengths: List[int]) -> str:
-        """
-        Build the very first line of the table.
-        :param col_lengths: A list of the longest lengths for each row (indexes matching).
-        :return: One line str of the top of the table.
-        """
+    def find_column_lengths(rows: List[List[Any]], n_cols: int, n_rows: int) -> List[int]:
+        col_lens = [0] * n_cols
+        for j in range(n_cols):
+            for i in range(n_rows):
+                if len(rows[i][j]) > col_lens[j]:
+                    col_lens[j] = len(rows[i][j])
+        return col_lens
+
+    def build_top_line(col_lens: List[int]) -> str:
         line = '┌'
-        for i, col_len in enumerate(col_lengths):
-            if i == len(col_lengths) - 1:
+        for i, col_len in enumerate(col_lens):
+            if i == len(col_lens) - 1:
                 # last index
                 line += ('─' * col_len) + '┐'
             else:
                 line += ('─' * col_len) + '┬'
-        return line
+        return line + '\n'
+
+    def build_headers(labels: List[Any], col_lens: List[int]):
+        line = '│'
+        for i, label in enumerate(labels):
+            col_len = col_lens[i]
+            line += f'{str(label):^{col_len}}|'
+        line += '\n'
+
+        line += '├'
+        for i, c_len in enumerate(col_lens):
+            if i == len(col_lens) - 1:
+                # last index
+                line += ('─' * c_len) + '┤'
+            else:
+                line += ('─' * c_len) + '┼'
+
+        return line + '\n'
 
     table = ''
     try:
-        num_cols = len(rows[0])
+        n_rows = len(rows)
+        n_cols = len(rows[0])
     except (TypeError, IndexError):
         # not sure how we want to handle errors like this yet? is this even possible with the type checking??
         return None
 
-    col_lengths = find_column_lengths(rows)
+    col_lens = find_column_lengths(rows, n_cols, n_rows)
 
-    table += build_top_line(col_lengths)
+    table += build_top_line(col_lens)
+
     if labels:
         # build headers
-        pass
+        table += build_headers(labels, col_lens)
 
     return table
 
 
 if __name__ == '__main__':
-    table = make_table([
-        ['hello', 'there',],
-        ['obi', 'wan'],
-        ['ooga', 'boogachooga'],
-    ])
+    table = make_table(
+        [['hello', 'there',], ['obi', 'wan'], ['ooga', 'boogachooga'],],
+        labels=['boss', 'baby']
+    )
     print(table)
